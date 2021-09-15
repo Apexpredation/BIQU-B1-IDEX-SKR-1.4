6dac26325e4d1bbdcf1515b10082ce492d38ad90
Apexpredation <apexpredation@gmail.com>
2021-09-15 21:11:07 +0200

    Added files

---
+++ Firmware/www/precache-manifest.08b4c61242ac3cb9e1bd2a96bd3aacb2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19ca666666b273928f9",
+    "url": "/css/GCodeViewer.68d05d8a.css"
+  },
+  {
+    "revision": "d47fb9f3dc40e6745f4b",
+    "url": "/css/HeightMap.389e8521.css"
+  },
+  {
+    "revision": "07d22a9adeae66cdad76",
+    "url": "/css/ObjectModelBrowser.c5e13b42.css"
+  },
+  {
+    "revision": "49d39d59bf6e6169f3d4",
+    "url": "/css/OnScreenKeyboard.578a974b.css"
+  },
+  {
+    "revision": "07f3a1c9a0de5079174c",
+    "url": "/css/app.54d98193.css"
+  },
+  {
+    "revision": "3d1f8fa2f06249540889a7bbe69cf5bb",
+    "url": "/fonts/materialdesignicons-webfont.3d1f8fa2.eot"
+  },
+  {
+    "revision": "3e722fd57a6db80ee119f0e2c230ccff",
+    "url": "/fonts/materialdesignicons-webfont.3e722fd5.ttf"
+  },
+  {
+    "revision": "4187121a4353440c2a865dbf1bc1901b",
+    "url": "/fonts/materialdesignicons-webfont.4187121a.woff2"
+  },
+  {
+    "revision": "fec1b66adcf131415a2511bd77e747cc",
+    "url": "/fonts/materialdesignicons-webfont.fec1b66a.woff"
+  },
+  {
+    "revision": "de11345487da955cfa3fec6c27176bc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b19ca666666b273928f9",
+    "url": "/js/GCodeViewer.928ca6b9.js"
+  },
+  {
+    "revision": "d47fb9f3dc40e6745f4b",
+    "url": "/js/HeightMap.7361cf24.js"
+  },
+  {
+    "revision": "07d22a9adeae66cdad76",
+    "url": "/js/ObjectModelBrowser.f722b29d.js"
+  },
+  {
+    "revision": "49d39d59bf6e6169f3d4",
+    "url": "/js/OnScreenKeyboard.1f9c0f92.js"
+  },
+  {
+    "revision": "07f3a1c9a0de5079174c",
+    "url": "/js/app.f43f66da.js"
+  },
+  {
+    "revision": "f5a3f67027690d7c10ad38afee1941f1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
